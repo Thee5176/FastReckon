b47e4b605a96fc964ba5870fb3de0b7ad61315e5
Thee5176 <Theerapong5176@gmail.com>
2024-12-26 11:43:00 +0900

    add fields to CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    GENDER_CHOICES = [
+        ('M','Male'),
+        ('F','Female'),
+    ]
+    
+    gender = models.CharField(choices=GENDER_CHOICES, null=True, blank=True)
+    occupancy = models.CharField(null=True, blank=True)
+    city = models.CharField(max_length=50)    #TODO:get dropdown choices
+    annual_income = models.PositiveIntegerField(null=True, blank=True)
